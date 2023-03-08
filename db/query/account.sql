dd3536db8ebb21aab48121f3d0937b87b44daa23
paulo <paulo.junior@4-sure.net>
2023-03-08 16:30:25 +0200

    init part design DB schema & CRUD with only CR

---
+++ db/query/account.sql
@@ -0,0 +1,22 @@
+-- name: CreateAccount :one
+INSERT INTO account (
+    owner,
+    balance,
+    currency
+) VALUES (
+    $1, $2, $3
+) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM account 
+WHERE id = $1 LIMIT 1;
+
+
+-- name: ListAccounts :many
+SELECT * FROM account 
+WHERE owner = id
+LIMIT $1 
+OFFSET $2;
+
+
+    
