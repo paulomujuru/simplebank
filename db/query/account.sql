c4ac246c25139c7ee08122c398cc3fe0ba1c809b
paulo <paulo.junior@4-sure.net>
2023-03-15 00:29:42 +0200

    CRUD for entries table

--- db/query/account.sql
+++ db/query/account.sql
@@ -13,10 +13,18 @@ WHERE id = $1 LIMIT 1;
 
 
 -- name: ListAccounts :many
-SELECT * FROM account 
-WHERE owner = id
-LIMIT $1 
+SELECT * FROM account
+ORDER BY id
+LIMIT $1
 OFFSET $2;
 
+-- name: UpdateAccount :one
+UPDATE account 
+SET balance = $2
+WHERE id = $1
+RETURNING *;
 
-    
+
+-- name: DeleteAccount :exec
+DELETE FROM account
+WHERE id = $1;
